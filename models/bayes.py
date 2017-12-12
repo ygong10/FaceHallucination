708930da07bf8f037c7ed3b10e272545ccea2909
Andrew Yang <ayang14@illinois.edu>
2017-12-12 12:57:09 -0600

    Sobel filters

--- models/bayes.py
+++ models/bayes.py
@@ -1,3 +1,32 @@
+import numpy as np
+from scipy.misc import imread
+
+# Sobel filters
+first_sobel_horiz = np.array([
+    [1, 0, -1],
+    [2, 0, -2],
+    [1, 0, -1]
+])
+
+first_sobel_vert = np.array([
+    [1, 2, 1],
+    [0, 0, 0],
+    [-1, -2, -1]
+])
+
+# https://dsp.stackexchange.com/a/10622
+second_sobel_horiz = np.array([
+    [1, -2, 1],
+    [2, -4, 2],
+    [1, -2, 1]
+])
+
+second_sobel_vert = np.array([
+    [1, 2, 1],
+    [-2, -4, -2],
+    [1, 2, 1]
+])
+
 def W(m, n, p, q, k):
     if m * 2**k <= p < (m+1) * 2**k and n * 2**k <= q < (n+1) * 2**k:
         return 1 / 2 ** (2*k)
@@ -15,6 +44,6 @@ def map_formulation(G_0, G_k, height, width, k=2, covariance=1.0):
             for n in range(width))
         for m in range(height))
 
-def gradient_prior(G_0, G_k, height, width, k=2, covariance=1.0):
+def gradient_prior(H_0, V_0, height, width, k=2, covariance=1.0):
     # calculates -ln Pr[G_0]
     pass
