dda1632f940b53b803617196b5073164866116e8
Yicheng Gong <ygongdev@gmail.com>
2017-12-12 23:18:57 -0600

    finished eigentransformation

--- eigentransform.py
+++ eigentransform.py
@@ -1,10 +1,10 @@
-import numpy as np 
+import numpy as np
 from PIL import Image
 from copy import deepcopy
-from scipy import ndimage
+import scipy.linalg
 import matplotlib.pyplot as plt
 
-def load_data(low_res_data_path, high_res_data_path):
+def load_data(low_res_file_path, high_res_file_path):
   low_res_images = np.load(low_res_file_path)
   high_res_images = np.load(high_res_file_path)
 
@@ -17,7 +17,7 @@ def mean_face(face_matrix):
 def eigenfaces(face_matrix):
   return face_matrix - mean_face(face_matrix)
 
-def orthonormal_eigenvectors(eigenfaces, eigenvectors, eigenvalues)
+def orthonormal_eigenvectors(eigenfaces, eigenvectors, eigenvalues):
   pass
 
 
@@ -28,11 +28,68 @@ def main():
   HIGH_RES_DATA_PATH = 'training/training_high_res.npy'
 
   low_res_images, high_res_images = load_data(LOW_RES_DATA_PATH, HIGH_RES_DATA_PATH)
-  
-  m_l = np.mean(high_res_images, axis=0)
-  L = high_res_images - m_l
-  LL_T = np.matmul(L, L.T)
-  # TO FINISH
+
+  input_image = low_res_images[:, 0]
+  # test = input_image.reshape(16,12)
+  # plt.imshow(test, cmap="gray")
+  # plt.show()
+
+  # PCA
+  m_l = np.mean(low_res_images, axis=1)
+  L = low_res_images - np.vstack(m_l)
+  R = np.matmul(L.T, L)
+  A_I, V_I = scipy.linalg.eig(R, left=True, right=False)
+  # Turn the eigenvalues into an eigenvalue matrix
+  A_I = np.eye(A_I.shape[0]) * A_I
+  # Numpy returns complex value for a few pixels.
+  E = np.real(np.matmul(np.matmul(L, V_I), (np.linalg.inv(A_I)**(1/2))))
+  #result = E[:, 0]
+  #result = result.reshape(29, 38)
+  w_l = np.matmul(E.T, input_image - m_l)
+  #result = np.matmul(E, w_l) + m_l
+  #print(result.shape)
+  # acc = 0
+  # for i in range((E.shape[1])):
+  #   if any(np.iscomplex(E[:,i])):
+  #     acc+= 1
+  # print(acc)
+  # Numpy returns 0 complex for some reason.
+  #result = np.real(E[:, 0])
+
+  #print(result[0,0])
+  #print(w_l.shape)
+  #result = np.matmul(E, w_l) + m_l
+  #result = result.reshape(454, 604)
+  # plt.imshow(result, cmap="gray")
+  # plt.show()
+  # Eigentransformation
+  m_h = np.mean(high_res_images, axis=1)
+  x_h = np.zeros((128 * 96,))
+  for i in range(len(w_l)):
+    c_i = w_l[i]
+    x_h += (c_i * high_res_images[:, i]) + m_h
+  L_h = high_res_images - np.vstack(m_h)
+  R_h = np.matmul(L_h.T, L_h)
+  A_I_h, V_I_h = scipy.linalg.eig(R_h, left=True, right=False)
+  A_I_h = np.eye(A_I_h.shape[0]) * A_I_h
+  E_h = np.real(np.matmul(np.matmul(L_h, V_I_h), (np.linalg.inv(A_I_h)**(1/2))))
+  #print(E_h.shape)
+  w_h = np.matmul(E_h.T, x_h - m_h)
+  print(w_h.shape)
+  alpha = 0.5
+
+  for i in range(len(w_h)):
+    w_h_i = w_h[i]
+    eigval = A_I_h[i,i]
+    if (w_h_i > (alpha * (eigval**(1/2)))):
+      w_h[i] = np.sign(w_h[i]) * (alpha * (eigval**(1/2)))
+
+  hallucinated_result = np.matmul(E_h, w_h) + m_h
+  hallucinated_result = hallucinated_result.reshape(128, 96)
+  plt.imshow(hallucinated_result, cmap="gray")
+  plt.show()
+
+
 main()
 
 
--- preprocess.py
+++ preprocess.py
@@ -1,8 +1,9 @@
 import os, os.path
-import numpy as np 
+import numpy as np
 from PIL import Image
 from copy import deepcopy
 from scipy import ndimage
+import scipy
 import matplotlib.pyplot as plt
 
 # Creates low resolution training images along with corresponding high resolution training_images
@@ -10,14 +11,14 @@ def create_data_from_images(data_path, low_res_file_path, high_res_file_path, si
   high_res_images = os.listdir(data_path)
   num_samples = len(high_res_images)
 
-  sample_image = convert_pgm_to_numpy(os.path.join(data_path, high_res_images[0]))
+  sample_image = scipy.misc.imread(os.path.join(data_path, high_res_images[0]), mode='L')
   width, height = sample_image.shape
   sample_pyramid = generate_gaussian_pyramid(
-      im=sample_image, 
-      downsample_factor=downsample_factor, 
-      sigma=sigma, 
+      im=sample_image,
+      downsample_factor=downsample_factor,
+      sigma=sigma,
       levels=levels)
-      
+
   low_res_width, low_res_height = sample_pyramid[-1].shape
   del sample_image
   del sample_pyramid
@@ -25,35 +26,35 @@ def create_data_from_images(data_path, low_res_file_path, high_res_file_path, si
   low_res_data = np.zeros((low_res_width * low_res_height, num_samples))
 
   for i in range(num_samples):
-    high_res_image = convert_pgm_to_numpy(os.path.join(data_path, high_res_images[i]))
+    high_res_image = scipy.misc.imread(os.path.join(data_path, high_res_images[i]), mode='L')
     pyramid = generate_gaussian_pyramid(
-      im=high_res_image, 
-      downsample_factor=downsample_factor, 
-      sigma=sigma, 
+      im=high_res_image,
+      downsample_factor=downsample_factor,
+      sigma=sigma,
       levels=levels)
     high_res_image = pyramid[0].flatten()
     low_res_image = pyramid[-1].flatten()
     high_res_data[:, i] = high_res_image
     low_res_data[:, i] = low_res_image
-    print ("Finished processing image" + str(i))
-  
+    print ("Finished processing image " + str(i))
+
   np.save(low_res_file_path, low_res_data)
   print("Saving low resolution training data")
   np.save(high_res_file_path, high_res_data)
   print("Saving high resolution training data")
   print("Done")
 
-# Converts a pgm image into a numpy ndarray
-def convert_pgm_to_numpy(pgm_img):
-  im = Image.open(pgm_img)
-  return np.array(im)
+# Converts an image into a numpy ndarray
+# def convert_image_to_numpy(pgm_img):
+#   im = scipy.misc.imread(pgm_img, mode='L')
+#   return np.array(im)
 
 # Downsample a numpy ndarray image by an integer factor
 def down_sample(im, factor):
   width, height = im.shape
   if (factor > width or factor > height):
     raise ValueError("Subsample factor is bigger than image dimensions")
-    
+
   im = im[::factor, ::factor]
   return im
 
@@ -75,23 +76,22 @@ def generate_gaussian_pyramid(im, downsample_factor, sigma, levels):
     im = ndimage.filters.gaussian_filter(im, sigma)
     im = down_sample(im, downsample_factor)
     gaussian_pyramid.append(im)
-  
+
   return gaussian_pyramid
 
-# im = convert_pgm_to_numpy("data/CAFE-FACS-Orig/004_d2.pgm")
-# print(im.shape)
-# pyramid = generate_gaussian_pyramid(im, 2, 2, 5)
-# plt.imshow(pyramid[4], cmap="gray")
-# plt.show()
-#create_data_from_images('data/CAFE-FACS-Orig')
-#save_data('training_high')
+im = scipy.misc.imread("aligned_data/ucsd_aligned_images/004_d2_aligned.png", mode='L')
+print(im.shape)
+pyramid = generate_gaussian_pyramid(im, 2, 2, 4)
+plt.imshow(pyramid[0], cmap="gray")
+plt.show()
 
-IMAGE_PATH = 'data/CAFE-FACS-Orig'
+
+IMAGE_PATH = 'aligned_data/ucsd_aligned_images/'
 TRAINING_HIGH_RES_PATH = 'training/training_high_res'
 TRAINING_LOW_RES_PATH = 'training/training_low_res'
 SIGMA = 2 # Sigma for gaussian filter.
 DOWNSAMPLE_FACTOR = 2 # Downsample factor for gaussian pyramid
-PYRAMID_LEVELS = 5 
+PYRAMID_LEVELS = 4
 
 # Run this to generate the training low resolution and high resolution images
 # create_data_from_images(
@@ -106,5 +106,11 @@ PYRAMID_LEVELS = 5
 # How to recover a 1d low res (4th level of gaussian pyramid) from .npy back to a 2d low res
 # test = np.load('training/training_low_res.npy')[:, 0]
 # test = test.reshape(29, 38)
+# plt.imshow(test, cmap="gray")
+# plt.show()
+
+#How to recover a 1d high res from .npy back to a 2d high res
+# test = np.load('training/training_low_res.npy')[:, 0]
+# test = test.reshape(454, 604)
 # plt.imshow(test, cmap="gray")
 # plt.show()
